5696a1a73eefcdf6414d4acdd7612206371660d6
Sean Price <seanprice@sevenhillstechnology.com>
2018-10-23 13:07:35 -0400

    Remove api key from config

--- config.ts
+++ config.ts
@@ -1,9 +1,4 @@
-const jiraUsername = "seanprice@sevenhillstechnology.com";
-const jiraApiKey = "5YBRbslcdecIOMToifDZ52A5";
-const jiraBase64Auth = Buffer.from(`${jiraUsername}:${jiraApiKey}`).toString("base64");
-const jiraAuthHeaderValue = `Basic ${jiraBase64Auth}`;
-
 export const config = {
     jiraApiBaseUrl: process.env.JIRA_API_BASE_URL || "https://sevenhillstechnology.atlassian.net/rest/api/3",
-    jiraApiAuthHeaderValue: process.env.JIRA_API_AUTH_HEADER_VALUE || jiraAuthHeaderValue
+    jiraApiAuthHeaderValue: process.env.JIRA_API_AUTH_HEADER_VALUE
 };
